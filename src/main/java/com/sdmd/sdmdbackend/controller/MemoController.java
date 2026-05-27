16a0ef55d5068c6df55208a4420228625238b6a1
Hyun_Jun <hyunjundev8904@gmail.com>
2026-05-27 18:34:25 +0900

    memo 조회, 등록, 삭제

--- src/main/java/com/sdmd/sdmdbackend/controller/MemoController.java
+++ src/main/java/com/sdmd/sdmdbackend/controller/MemoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/memo")
 public class MemoController {
-
+    // 커밋용 주석
     private final MemoService service;
 
     // 등록
